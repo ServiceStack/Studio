66d481d7e2f82fbb13ed038f49d4e5ff8632e396
Demis Bellot <demis.bellot@gmail.com>
2020-04-27 18:25:21 +0800

    update scripts

---
+++ pack.bat
@@ -0,0 +1,9 @@
+rem dotnet publish -c release
+rd /q /s dist
+md dist\plugins dist\assets
+x run _bundle.ss
+copy bin\release\netcoreapp3.1\publish\Studio.dll dist\plugins\
+xcopy /E bin\release\netcoreapp3.1\publish\wwwroot\assets .\dist\assets\
+copy app.settings dist
+copy wwwroot\* dist
+
--- publish.bat
+++ publish.bat
@@ -1,8 +1,3 @@
-rem dotnet publish -c release
-rd /q /s dist
-md dist\plugins dist\assets
-x run _bundle.ss
-copy bin\release\netcoreapp3.1\publish\Studio.dll dist\plugins\
-xcopy /E bin\release\netcoreapp3.1\publish\wwwroot\assets .\dist\assets\
-copy app.settings dist
-copy wwwroot\* dist
+pushd dist
+app publish -token %GISTLYN_TOKEN%
+popd
